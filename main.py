7a012f1569ad904413235e0fb9c32017d2c7cf25
YuvrajMangutkar <mangutkaryuvraj@gmail.com>
2025-02-19 18:52:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ song_names_spans=soup.select("li ul li h3")
 # print(song_names_spans)
 song_names=[ song.getText().strip() for song in song_names_spans]
 print(song_names)
-Client_id="d44685189c064ed8b40a737538b90352"
-Client_secret="10867f78ce6b4c3781fca6ab85eeb19a"
+Client_id="YOUR SPOTIFY CLIENT ID"
+Client_secret="YOUR SPOTIFY CLIENT SECRET"
 sp=spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
@@ -22,7 +22,7 @@ sp=spotipy.Spotify(
         client_secret=Client_secret,
         show_dialog=True,
         cache_path="token.txt",
-        username="31jlexnqx3tvciprkzlirwyta2ya"
+        username="YOUR SPOTIFY ACCOUNT USERNAME"
 
     )
 
@@ -40,6 +40,6 @@ for song in song_names:
     except IndexError:
         print(f"{song} doesn't exist in spotify. Skipped.")
 
-playlist=sp.user_playlist_create(user='31jlexnqx3tvciprkzlirwyta2ya',name=f"{date} Billboard 100",public=False)
+playlist=sp.user_playlist_create(user='SPOTIFY ACCOUNT USERNAME',name=f"{date} Billboard 100",public=False)
 print(playlist)
 sp.playlist_add_items(playlist_id=playlist["id"],items=song_uris)
